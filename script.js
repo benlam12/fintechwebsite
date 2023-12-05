fb15f07434581c087cca40b1f8050d072c296471
benlam12 <57205951+benlam12@users.noreply.github.com>
2023-12-05 08:24:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,94 @@ function hashData(data) {
     return CryptoJS.SHA256(data).toString(CryptoJS.enc.Hex);
 }
 
+// Event listener for form submission
 const identityForm = document.getElementById("identityForm");
 const myPopup = document.getElementById("myPopup");
 const closePopup = document.getElementById("closePopup");
 const hashedDataDisplay = document.getElementById("hashedData");
+
+// Initialize Web3 with your provider
+const web3 = new Web3('http://localhost:8545'); // Update with your provider URL
+
+// Contract address and ABI
+const contractAddress = 'YOUR_CONTRACT_ADDRESS'; // Replace with your deployed contract address
+const contractABI = [[
+                      {
+                        "inputs": [
+                          {
+                            "internalType": "string",
+                            "name": "_name",
+                            "type": "string"
+                          },
+                          {
+                            "internalType": "string",
+                            "name": "_gender",
+                            "type": "string"
+                          },
+                          {
+                            "internalType": "string",
+                            "name": "_birthday",
+                            "type": "string"
+                          },
+                          {
+                            "internalType": "string",
+                            "name": "_phoneNumber",
+                            "type": "string"
+                          },
+                          {
+                            "internalType": "string",
+                            "name": "_email",
+                            "type": "string"
+                          }
+                        ],
+                        "name": "storeUserData",
+                        "outputs": [],
+                        "stateMutability": "nonpayable",
+                        "type": "function"
+                      },
+                      {
+                        "inputs": [
+                          {
+                            "internalType": "address",
+                            "name": "",
+                            "type": "address"
+                          }
+                        ],
+                        "name": "userRecords",
+                        "outputs": [
+                          {
+                            "internalType": "string",
+                            "name": "name",
+                            "type": "string"
+                          },
+                          {
+                            "internalType": "string",
+                            "name": "gender",
+                            "type": "string"
+                          },
+                          {
+                            "internalType": "string",
+                            "name": "birthday",
+                            "type": "string"
+                          },
+                          {
+                            "internalType": "string",
+                            "name": "phoneNumber",
+                            "type": "string"
+                          },
+                          {
+                            "internalType": "string",
+                            "name": "email",
+                            "type": "string"
+                          }
+                        ],
+                        "stateMutability": "view",
+                        "type": "function"
+                      }
+                    ]]; // Replace with your contract's ABI
+
+// Create a contract instance
+const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 identityForm.addEventListener("submit", function(e) {
     e.preventDefault();
@@ -34,15 +118,25 @@ identityForm.addEventListener("submit", function(e) {
         // Display the hashed value in the popup
         hashedDataDisplay.textContent = "Hashed Data:\n" + hashedData;
         myPopup.classList.add("show");
+
+        // Send hashed data to the smart contract
+        sendToBlockchain(hashedData);
     }
 });
 
-closePopup.addEventListener("click", function () {
-    myPopup.classList.remove("show");
-});
+// Function to send data to the smart contract
+function sendToBlockchain(hashedData) {
+    contract.methods.storeUserData(hashedData)
+        .send({ from: 'YOUR_SENDER_ADDRESS' }) // Replace with the sender's address
+        .on('transactionHash', function(hash){
+            console.log('Transaction Hash:', hash);
+            // You can perform actions after the transaction is mined
+        })
+        .on('error', function(error){
+            console.error('Error:', error);
+            // Handle errors during the transaction
+        });
+}
 
-window.addEventListener("click", function (event) {
-    if (event.target == myPopup) {
-        myPopup.classList.remove("show");
-    }
-});
+// Rest of your code (popup handling, event listeners, etc.)
+// ...
