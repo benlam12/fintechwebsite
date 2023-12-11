b7c14294ad3b5aa056f4f8a6506f609e9f2c0b96
benlam12 <57205951+benlam12@users.noreply.github.com>
2023-12-11 18:10:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,55 @@
        $(this).trigger("reset");
     });
 
+    $.ajax({
+        type: 'POST',
+        url: '/saveData', // Replace with your server endpoint
+        data: {
+          formData: formData,
+          hashedKey: hashedData
+        },
+        success: function (response) {
+          $('#popupForm').show();
+          $(this).trigger("reset");
+        },
+        error: function (error) {
+          console.error('Error:', error);
+        }
+      });
+
+
+    // Handle key verification
+    $('#verify-key-button').click(async function () {
+      const keyToVerify = $('#key').val();
+
+      // Send key verification request to server
+      try {
+        const response = await fetch('/verifyKey', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ key: keyToVerify })
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          if (result.success) {
+            const formData = result.formData;
+            // Display or use the verified form data as needed
+            console.log('Verified Form Data:', formData);
+          } else {
+            alert('Key not found');
+          }
+        } else {
+          console.error('Failed to verify key');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    });
+
+
     // Handle close popup button click
     $('#closePopup').click(function (event) {
       event.preventDefault();
