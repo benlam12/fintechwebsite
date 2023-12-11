8b3194e74913a0e0f5d2de04b88c76353a511773
benlam12 <57205951+benlam12@users.noreply.github.com>
2023-12-11 20:54:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,89 @@
-  import sqlite3 from 'sqlite3';
-  
-  $(document).ready(function () {
-    // Handle submit button click
-    $('#dynamicForm').submit(function (event) {
-      event.preventDefault();
+$(document).ready(function () {
 
-      // Collect form data
-      const formData = {
-        name: $('#name').val(),
-        email: $('#email').val(),
-        birthday: $('#birthday').val(),
-        gender: $('#gender').val(),
-        phone_number: $('#phone_number').val(),
-      };
+  // Define function to write data to a CSV file
+  function writeCsvData(filename, data) {
+    const csvString = Papa.unparse(data);
+    const blob = new Blob([csvString], { type: "text/csv;charset=utf-8" });
+    saveAs(blob, filename);
+  }
 
-      const hashedData = CryptoJS.SHA256(JSON.stringify(formData)).toString();
+  // Handle submit button click
+  $('#dynamicForm').submit(function (event) {
+    event.preventDefault();
 
-      $('#hashedKey').text(hashedData);
+    // Collect form data
+    const formData = {
+      name: $('#name').val(),
+      email: $('#email').val(),
+      birthday: $('#birthday').val(),
+      gender: $('#gender').val(),
+      phone_number: $('#phone_number').val(),
+    };
 
-      // Show popup form
-      $('#popupForm').show();
+    // Hash the data
+    const hashedData = CryptoJS.SHA256(JSON.stringify(formData)).toString();
 
-      
-       $(this).trigger("reset");
-    });
+    // Combine data with hash
+    const combinedData = { ...formData, hashedKey: hashedData };
 
-    // Handle close popup button click
-    $('#closePopup').click(function (event) {
-      event.preventDefault();
+    // Write data to a local CSV file
+    writeCsvData("form_data.csv", [combinedData]);
 
-      // Hide popup form
-      $('#popupForm').hide();
+    // Show popup form
+    $('#popupForm').show();
+
+    // Store hashed key in popup
+    $('#hashedKey').text(hashedData);
+
+    // Clear form
+    $(this).trigger("reset");
+  });
+
+  // Handle verify key button click
+  $('#verify-key-button').click(function (event) {
+    event.preventDefault();
+
+    // Get entered key
+    const enteredKey = $('#key').val();
+
+    // Read data from the local CSV file
+    Papa.parse("form_data.csv", {
+      download: true,
+      complete: function (results) {
+        const data = results.data;
+        const record = data.find(record => record.hashedKey === enteredKey);
+
+        if (record) {
+          // Key is valid, show form data
+          $('#verification-container').hide();
+          $('#popupForm').show();
+          $('#hashedKey').text('Identity Verified');
+
+          // Display form data in popup
+          $('#popupForm').empty();
+          $('#popupForm').append(`
+            <h2>Form Data</h2>
+            <ul>
+              <li>Name: ${record.name}</li>
+              <li>Email: ${record.email}</li>
+              <li>Birthday: ${record.birthday}</li>
+              <li>Gender: ${record.gender}</li>
+              <li>Phone Number: ${record.phone_number}</li>
+            </ul>
+          `);
+        } else {
+          alert('Invalid key!');
+        }
+      },
     });
   });
+
+  // Handle close popup button click
+  $('#closePopup').click(function (event) {
+    event.preventDefault();
+
+    // Hide popup form
+    $('#popupForm').hide();
+    $('#verification-container').show();
+  });
+});
